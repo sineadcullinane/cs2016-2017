05496fed2893e4133bc27a9224b5bbc0689a71d6
Sinead Cullinane <sineadcullinane@users.noreply.github.com>
2017-06-06 12:54:02 +0100

    Create Feline.java

---
+++ semester1/cs2514/lab-3/Feline.java
@@ -0,0 +1,11 @@
+/**
+ * Feline abstract class that implements Animal interface
+ *
+ * @author Sinead Cullinane
+ */
+public abstract class Feline implements Animal {
+    public final int hungerLevel;
+    public final String picture;
+    public final boolean eatsGrass;
+}
+    
