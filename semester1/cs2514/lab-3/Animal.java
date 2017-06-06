286184a4f9e88b10de7945c138370ebd8d61c922
Sinead Cullinane <sineadcullinane@users.noreply.github.com>
2017-06-06 12:53:03 +0100

    Create Animal.java

---
+++ semester1/cs2514/lab-3/Animal.java
@@ -0,0 +1,13 @@
+/**
+ * Animal interface outlining interface methods of an animal
+ *
+ * @author Sinead Cullinane
+ */
+public interface Animal {
+    public void eat( );
+    public String food( );
+    public void makeNoise( );
+    public void roam( );
+}
+    
+    
