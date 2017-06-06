c2ee0a2c3b29a73eaea0651fb504be18511847e5
Sinead Cullinane <sineadcullinane@users.noreply.github.com>
2017-06-06 12:53:32 +0100

    Create Canine.java

---
+++ semester1/cs2514/lab-3/Canine.java
@@ -0,0 +1,10 @@
+/**
+ * Canine abstarct class that implements Animal interface
+ *
+ * @author Sinead Cullinane
+ */
+public abstract class Canine implements Animal {
+    public final int hungerLevel;
+    public final String picture;
+    public final boolean eatsGrass;     
+}
